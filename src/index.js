21b69c3f16589e45c308a34f9bbd1da2b2b7fffd
artem1426 <bsed00@mail.ru>
2022-02-01 20:01:51 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import getProductList from "./mock/data.js";
 import renderGoodsList from "./showcase.js";
+import './scss/main.scss';
 
 const productList = getProductList(20);
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,64 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
 
 module.exports = {
+    mode: "production",
     entry: {
         main: path.resolve(__dirname, './src/index.js'),
     },
 
     output: {
-      path: path.resolve(__dirname, './dist'),
-      filename: '[name].bundle.js',
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].bundle.js',
     },
 
     plugins: [
-      new HtmlWebpackPlugin({
-          title: 'GeekBrains Shop JS2',
-          template: path.resolve(__dirname, './public/template.html'), // шаблон
-          filename: 'index.html', // название выходного файла
-      }),
+        new HtmlWebpackPlugin({
+            title: 'GeekBrains Shop',
+            template: path.resolve(__dirname, './public/template.html'), // шаблон
+            filename: 'index.html', // название выходного файла
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+        }),
+        new CleanWebpackPlugin(),
+        new ImageminPlugin({
+            test: /\.(jpe?g|png|gif|svg)$/i
+        }),
     ],
-}
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: { sourceMap: true }
+                    }, {
+                        loader: 'sass-loader',
+                        options: { sourceMap: true }
+                    }
+                ],
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: 'asset/inline',
+            },
+        ],
+    },
+    optimization: {
+        minimizer: [
+            new CssMinimizerPlugin(),
+        ],
+    },
+};
